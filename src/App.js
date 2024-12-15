3795c56a4b4ebbd770ab62b43d84e7e6852617d4
Teddy Maulana <maulanateddy99@gmail.com>
2024-12-15 18:33:32 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './assets/zlap3.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App ">
+      <header className="App-header !bg-[#eb6e00]">
+        <img src={logo} className="" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
