36b935106597c8e8b1e67a96aed96cfbe9ceed2d
Teddy Maulana <maulanateddy99@gmail.com>
2024-12-21 22:44:09 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './assets/zlap3.png';
+import logo from './assets/logozlap2.png';
 import './App.css';
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
     <div className="App ">
       <header className="App-header !bg-[#eb6e00]">
         <img src={logo} className="" alt="logo" />
+        <a className='border-4 font-bold rounded-[10px] py-2 px-4 mb-4' href="https://www.tokopedia.com/zlap">Shop on Tokopedia</a>
+        <a className='border-4 font-bold rounded-[10px] py-2 px-4' href="https://www.tiktok.com/@zlap.collectibles">Visit Our Tiktok</a>
       </header>
     </div>
   );
