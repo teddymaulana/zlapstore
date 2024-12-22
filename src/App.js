e8990d75055f3bce453b7e9b0fbd1d91f1ebe557
Teddy Maulana <maulanateddy99@gmail.com>
2024-12-22 19:40:39 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="w-[150px]" alt="logo" />
         <img className="mt-[-50px] w-[80%]" src={promo} alt="promo" />
         <a className='border-4 font-bold rounded-[10px] py-2 px-4 mb-4' href="https://www.tokopedia.com/zlap">Shop on Tokopedia</a>
+        <a className='border-4 font-bold rounded-[10px] py-2 px-4 mb-4' href="https://shopee.co.id/shop/18959012">Shop on Shopee</a>
         <a className='border-4 font-bold rounded-[10px] py-2 px-4' href="https://www.tiktok.com/@zlap.collectibles">Visit Our Tiktok</a>
       </header>
     </div>
