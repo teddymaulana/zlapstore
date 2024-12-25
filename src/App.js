65070d6fbbc0bf26f50b81a2725baa3cda6cf892
Teddy Maulana <maulanateddy99@gmail.com>
2024-12-25 23:24:20 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import logo from './assets/logozlap2.png';
-import promo from './assets/3packs.png';
+import promo from './assets/terafest1.png';
+import promo2 from './assets/terafest-pack.png';
 import './App.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 function App() {
   return (
     <div className="App ">
-      <header className="App-header !bg-[#eb6e00]">
+      <header className={`App-header bg-cover bg-[url('./assets/bg.png')]`} style={{ backgroundImage: './assets/bg.png'}}>
         <img src={logo} className="w-[150px]" alt="logo" />
-        <img className="mt-[-50px] w-[80%]" src={promo} alt="promo" />
-        <a className='border-4 font-bold rounded-[10px] py-2 px-4 mb-4' href="https://www.tokopedia.com/zlap">Shop on Tokopedia</a>
-        <a className='border-4 font-bold rounded-[10px] py-2 px-4 mb-4' href="https://shopee.co.id/shop/18959012">Shop on Shopee</a>
-        <a className='border-4 font-bold rounded-[10px] py-2 px-4' href="https://www.tiktok.com/@zlap.collectibles">Visit Our Tiktok</a>
+        {/* 
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={1}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className='w-[200px] m-[20px]'
+        >
+          <SwiperSlide>
+            <img src={promo} className='w-full'/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={promo2} className='w-full'/>
+          </SwiperSlide>
+        </Swiper>
+        */}
+        <a className='border-4 font-bold bg-[#1b434c] rounded-[10px] py-2 px-4 mb-4 w-[220px]' href="https://www.tokopedia.com/zlap">Shop on Tokopedia</a>
+        <a className='border-4 font-bold bg-[#1b434c] rounded-[10px] py-2 px-4 mb-4 w-[220px]' href="https://shopee.co.id/shop/18959012">Shop on Shopee</a>
+        <a className='border-4 font-bold bg-[#1b434c] rounded-[10px] py-2 px-4 w-[220px]' href="https://www.tiktok.com/@zlap.collectibles">Visit Our Tiktok</a>
       </header>
     </div>
   );
